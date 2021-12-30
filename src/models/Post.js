902883005d897e58966e036f2c74cf0ef0062c9f
MateusBeltrao <mateusbeltrao2019@gmail.com>
2021-12-30 20:42:37 -0300

    first commit

---
+++ src/models/Post.js
@@ -0,0 +1,22 @@
+const mongoose = require('../db')
+require('dotenv').config()
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    content: {
+        type: String,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+
+const Post = mongoose.model('Post', PostSchema)
+
+module.exports = Post
